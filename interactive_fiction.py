78fdfea7585975e448dce2a9d288087098962449
earendelmir <earendelmir@gmail.com>
2020-09-02 15:00:29 +0200

    changed the style of some parts of the code

--- interactive_fiction.py
+++ interactive_fiction.py
@@ -4,110 +4,88 @@
 #
 from os import system
 
-# to change in "cls" if running on Windows
-CLEAR_SCREEN = "clear"
-
 
 class Action():
-	def __init__(self, description, consequence):
-		# every action has a description that will be printed out for the
-		# player to get a sense of what the action does
-		self.description = description
-		# this value represents the identificative number of the scene that 
-		# has to be displayed when the user chooses this particolar action
+	def __init__(self, prompt, consequence):
+		# every action has a prompt that will be printed out for the player to get a sense of what the action does
+		self.prompt = prompt
+		# this is the identificative number of the scene that has to be displayed when the user chooses this particolar action
 		self.consequence = consequence
-	
-	# prints the index of the action (n) and its description
-	# (e.g. "1. go north")
-	def print(self, n):
-		print("{}. {}".format(n, self.description))
+
+	# prints the index of the action (n) and its prompt (e.g. "1. go north")
+	def show(self, n):
+		print("{}. {}".format(n, self.prompt))
 
 
 
 class Scene():
-	def __init__(self, id, status, title, description, actions):
-		# each scene has its own identificative number
-		self.id = id
-		# this value is != 0 when the game ends in the scene
-		# it is 0 in any other case
-		self.game_status = status
-		# title of the scene to be printed out
-		self.title = title
-		# description of the scene to be printed out
-		self.description = description
-		# the array of actions available in the scene
-		self.actions = actions
-	
+	def __init__(self, ID, status, title, description, actions):
+		self.id = ID                   # each scene has its own identificative number
+		self.status = status           # this value is != 0 when the game ends in the scene
+		self.title = title             # title of the scene to be printed out
+		self.description = description # description of the scene to be printed out
+		self.actions = actions         # the array of actions available in the scene
+
 	# prints the current scene
-	def print(self):
-		# clears the screen from the previous output
-		system(CLEAR_SCREEN)
-		# prints the title
+	def show(self):
+		# clears the screen before printing the new scene.
+		system("clear") # change "clear" with "cls" if running on Windows
 		print(self.title, end="\n\n")
-		# prints the description
 		print(self.description, end="\n\n")
-		# and the actions
 		for i in range(len(self.actions)):
-			self.actions[i].print(i+1)
-		
-	# returns true if the game_status property is other than 0
-	def game_over(self):
-		return (self.game_status != 0)
+			self.actions[i].show(i+1)
+
+	# returns true if the status property is other than 0
+	def gameOver(self):
+		return (self.status != 0)
 
 
 
-# this function takes a number in input and returns the correspondent
-# index of the action choosen by the user
-def user_input(num_actions):
+# this function takes a number in input and returns the correspondent index of the action choosen by the user
+def userInput(num_actions):
 	# gets the string in input
 	string = input("> ")
+	# it tries converting the string into an integer. This fails if the string is not a number or if it is empty
 	try:
-		# tries converting the string into an integer
 		n = int(string, base=10)
-	# but in case the string was not a number (or if it was empty)
 	except ValueError:
-		# it prints the error and asks again
 		print("Invalid choice. You must choose a number between 1 and", num_actions)
-		return user_input(num_actions)
+		return userInput(num_actions)
 
 	# checks that the input is both > 0 and < num_actions
 	if (n < 1 or n > num_actions):
 		print("Invalid choice. You must choose a number between 1 and", num_actions)
-		return user_input(num_actions)
+		return userInput(num_actions)
 	else:
-		# this function returns n-1 because the user can choose between 1 and N
-		# but in the array, the 1st element is at index 0
+		# this function returns n-1 because even though the user can choose between 1 and N, the 1st element is at index 0
 		return n-1
 
 
 
-# searches for the scene with the new_id in the array 
-def next_scene(scenes, new_id, curr_id):
-	# for every scene
+# searches for the scene with the newID in the array
+def nextScene(scenes, newID, currID):
 	for i in range(len(scenes)):
-		# if its id matches the one 
-		if (scenes[i].id == new_id):
-			# it is returned
+		if (scenes[i].id == newID):
 			return scenes[i]
-		# if its id matches the one of the current scene it saves that scene
-		# into a variable
-		elif (scenes[i].id == curr_id):
+		elif (scenes[i].id == currID):
 			currentScene = scenes[i]
-	# the current scene is returned in case no match is found with the new id
-	# this might be caused by the user re-writing the story and messing up the ids
+	# the current scene is returned in case no match is found with the new ID
+	# this might be caused by the user re-writing the story and missing one or more IDs
 	return currentScene
 
 
 
-# this function prints the end credits once the game is completed
-def my_credits():
+# This function prints the end credits once the game is completed
+def myCredits():
 	print("\nA GAME DEVELOPED BY Eärendelmir\nGITHUB: https://github.com/earendelmir", end="\n\n")
 	print("This is just a demo story. You can create your own by following the instructions in the README.md file.", end="\n\n")
 
 
 
 
-# the array of scenes
+# The array of scenes. Only the 9th one has its status set to something other than 0 because it is the only scene which leads to the completion of
+# the game. If you intend to rewrite the story please make sure to include one - or more, even better - scenes which lead to the end of your game.
+# That way you'll be sure the player won't be stuck in the loop forever.
 SCENES = [
 	Scene(0, 0, "IN THE FOREST", "You wake up in the middle of a forest. You don't recall how you got here.\nOn your left there's a path leading up a hill. Right in front of you there's a chest.", [Action("Open chest.", 1), Action("Walk towards the top of the hill.", 3)]),
 	Scene(1, 0, "UNUSUAL GIFTS", "You open the chest with small effort thus revealing a small bottle containing a strange blue liquid. You take it.\nThere's a note attached to the bottle; it reads: \"Drink carefully. It is not what it seems.\"", [Action("Walk towards the top of the hill.", 4), Action("Drink liquid.", 2)]),
@@ -131,14 +109,10 @@ if __name__ == "__main__":
 	currentScene = SCENES[0]
 
 	while (True):
-		# prints the current scene
-		currentScene.print()
-		# checks wether it is the "last" (if the game is over) and, if it is, the loop gets broken
-		if (currentScene.game_over()):
+		currentScene.show()
+		if (currentScene.gameOver()):
 			break
-		# gets in input the choice for the action to perform
-		choice = user_input(len(currentScene.actions))
-		# updates the value of the current scene by searching for the next one in the array
-		currentScene = next_scene(SCENES, currentScene.actions[choice].consequence, currentScene.id)
-	
-	my_credits()
+		choice = userInput(len(currentScene.actions))
+		currentScene = nextScene(SCENES, currentScene.actions[choice].consequence, currentScene.id)
+
+	myCredits()
